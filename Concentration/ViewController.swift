c5d18e743f1674952d945f118594438bcb5d305e
Ramy Zhang <ramyjzh@gmail.com>
2019-12-25 15:17:14 -0500

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Ramy Zhang on 2019-12-25.
+//  Copyright © 2019 Ramy Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
